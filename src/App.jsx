6a8bc01f6611c2362c5b264f9070f473abee3e6f
Mohit Gusain <mohitgusain8671@gmail.com>
2025-06-28 13:15:47 +0530

    Adding menu and Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from './components/Hero';
 import Cocktail from './components/Cocktail';
 import About from './components/About';
 import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
       <Cocktail />
       <About />
       <Art/>
+      <Menu />
+      <Contact />
     </main>
   )
 }
